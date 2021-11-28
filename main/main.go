50c7430e704a791ec40daeebbe5c514cdf09a9c8
jes <sample@gmail.com>
2021-11-28 17:38:21 +0800

    maintest

---
+++ main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocurr/good"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+var nameFns = good.NameFns{
+	{"demo1", func() {
+		fmt.Println("demo1...")
+	}},
+
+	{"demo2", func() {
+		fmt.Println("demo2...")
+	}},
+}
+
+func main() {
+	c := good.Configure("./app.yml", false)
+	if err := good.StartCrontab(nameFns); err != nil {
+		log.Fatalln(err)
+	}
+
+	good.ServerMux(http.NewServeMux())
+	good.Route("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	good.Fire(c)
+}
