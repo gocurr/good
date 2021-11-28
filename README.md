aa1a591b2cc6a9ce6bd90cb54bb03dd5713b9fe5
jes <sample@gmail.com>
2021-11-28 16:03:05 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# The Go App Boot Framework
+
+`good` is a http framework that makes developers write go applications much easier.
+
+## Download and Install
+```bash
+go get -u github.com/gocurr/good
+```
+
+## Usage
+
+```go
+var nameFns = NameFns{
+	{"demo1", func() {
+		fmt.Println("demo1...")
+	}},
+
+	{"demo2", func() {
+		fmt.Println("demo2...")
+	}},
+}
+good.Configure("./app.yml", false)
+if err := good.StartCrontab(nameFns); err != nil {
+    panic(err)
+}
+
+good.ServerMux(http.NewServeMux())
+good.Route("/", func(w http.ResponseWriter, r *http.Request) {
+    _, _ = w.Write([]byte("ok"))
+})
+good.Fire()
+```
--- fire.go
+++ fire.go
@@ -9,6 +9,7 @@ import (
 
 var serverMux *http.ServeMux
 
+// Fire http server fire entry
 func Fire() {
 	if !configured {
 		log.Fatalln("Configure the application first!")
@@ -21,17 +22,21 @@ func Fire() {
 	port := conf.Server.Port
 	if port < 0 || port > 1<<16-1 {
 		log.Fatalln("Illegal server port!")
+	} else {
+		log.Infof(fmt.Sprintf("http server listening at [::%v]", port))
 	}
 
-	if err := http.ListenAndServe(":"+strconv.Itoa(port), serverMux); err == nil {
-		log.Infof(fmt.Sprintf("http server listening at [::%v]", port))
+	if err := http.ListenAndServe(":"+strconv.Itoa(port), serverMux); err != nil {
+		log.Errorf("http server: %v", err)
 	}
 }
 
+// ServerMux set serverMux
 func ServerMux(mux *http.ServeMux) {
 	serverMux = mux
 }
 
+// Route binds route path to fn
 func Route(route string, fn func(http.ResponseWriter, *http.Request)) {
 	serverMux.HandleFunc(route, fn)
 }
