52212d130a566161c1639263904457b0d434c527
jes <sample@gmail.com>
2021-12-04 16:01:54 +0800

    crypto

--- sugar/crypto.go
+++ sugar/crypto.go
@@ -14,8 +14,8 @@ var hexes = []rune{
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
 }
 
-// Key returns a secret key
-func Key() string {
+// newKey returns a secret newKey
+func newKey() string {
 	rand.Seed(time.Now().UnixNano())
 	var builder strings.Builder
 	for i := 0; i < 32; i++ {
@@ -25,27 +25,27 @@ func Key() string {
 	return builder.String()
 }
 
-// PrintKeyEnc prints secret-key and encrypted string
+// PrintKeyEnc prints secret-newKey and encrypted string
 func PrintKeyEnc(pw, filename string, reset ...bool) {
-	var secret string
+	var key string
 	if len(reset) > 0 && reset[0] {
 		_ = os.Remove(filename)
 	}
 
 	all, err := ioutil.ReadFile(filename)
 	if err != nil {
-		secret = Key()
-		if err := ioutil.WriteFile(filename, []byte(secret), os.ModePerm); err != nil {
+		key = newKey()
+		if err := ioutil.WriteFile(filename, []byte(key), os.ModePerm); err != nil {
 			panic(err)
 		}
 	} else {
-		secret = string(all)
+		key = string(all)
 	}
 
-	encrypted, err := crypto.Encrypt(secret, pw)
+	encrypted, err := crypto.Encrypt(key, pw)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("secret: %s\n", secret)
+	fmt.Printf("key: %s\n", key)
 	fmt.Printf("encrypted: %s\n", encrypted)
 }
