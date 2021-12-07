124532f8620e27d66d5a64f3a5a65fa7cc58a298
jes <sample@gmail.com>
2021-12-07 23:28:04 +0800

    streaming

---
+++ main/stream_test.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocurr/good/streaming"
+	"math"
+	"testing"
+)
+
+type Value struct {
+	val float64
+}
+
+type Values []*Value
+
+func Test_Values(t *testing.T) {
+	var vs []*Value = Values{&Value{val: 1}, &Value{val: 2}, &Value{val: 0}}
+	stream, err := streaming.Of(vs)
+	if err != nil {
+		return
+	}
+	stream.Limit(5).ForEach(func(i interface{}) {
+		fmt.Printf("%v\n", *i.(*Value))
+	})
+
+	fmt.Println()
+
+	stream.Map(func(i interface{}) interface{} {
+		return (*i.(*Value)).val * 100
+	}).ForEach(func(i interface{}) {
+		fmt.Printf("%v\n", i)
+	})
+	fmt.Printf("%v\n\n", stream.Count())
+	fmt.Printf("%v\n\n", stream.Filter(func(i interface{}) bool {
+		return i.(*Value).val > 1
+	}).Count())
+
+	stream.Limit(3).ForEach(func(i interface{}) {
+		fmt.Printf("%v\t", i.(*Value).val)
+	})
+}
+
+func TestWrap(t *testing.T) {
+	s, err := streaming.Of([]int{1, 2, 3})
+	if err != nil {
+		return
+	}
+	fmt.Printf("%v\n", s)
+}
+
+func Test_Filter(t *testing.T) {
+	s, err := streaming.Of([]int{1, 2, 3})
+	if err != nil {
+		return
+	}
+	filter := s.Filter(func(i interface{}) bool {
+		return i.(int) > 2
+	})
+	fmt.Printf("%v\n", filter)
+}
+
+func Test_Collect(t *testing.T) {
+	s, err := streaming.Of([]int{1, 2, 3})
+	if err != nil {
+		return
+	}
+	collect := s.Filter(func(i interface{}) bool {
+		return i.(int) > 2
+	}).Collect()
+	fmt.Printf("%v\n", collect)
+}
+
+func Test_Map(t *testing.T) {
+	s, err := streaming.Of([]int{1, 2, 3})
+	if err != nil {
+		return
+	}
+	collect := s.Map(func(i interface{}) interface{} {
+		return i.(int) * 2
+	}).Collect()
+	fmt.Printf("%v\n", collect)
+}
+
+func Test_ForEach(t *testing.T) {
+	s, err := streaming.Of([]int{1, 2, 3})
+	if err != nil {
+		return
+	}
+	s.ForEach(func(i interface{}) {
+		fmt.Printf("%v\n", i)
+	})
+}
+
+func Test_Limit(t *testing.T) {
+	s, err := streaming.Of([]int{1, 2, 3})
+	if err != nil {
+		return
+	}
+	s.Limit(2).ForEach(func(i interface{}) {
+		fmt.Printf("%v\n", i)
+	})
+}
+
+func Test_Array(t *testing.T) {
+	var arr = [...]int{1, 2, 3}
+	s, err := streaming.Of(arr)
+	if err != nil {
+		return
+	}
+	s.Map(func(i interface{}) interface{} {
+		return math.Pow(float64(i.(int)), 2)
+	}).ForEach(func(i interface{}) {
+		fmt.Printf("%v\n", i)
+	})
+	fmt.Println()
+	println(s.Count())
+}
+
+func TestStream_Reduce(t *testing.T) {
+	s, err := streaming.Of([]int{11, 152, 113})
+	if err != nil {
+		return
+	}
+
+	reduce, i := s.Reduce(func(a, b interface{}) bool {
+		return a.(int) > b.(int)
+	})
+	fmt.Println(reduce, i)
+}
+
+func Test_nil(t *testing.T) {
+	var raw []int
+	stream, err := streaming.Of(raw)
+	if err != nil {
+		return
+	}
+
+	println(stream.Map(func(i interface{}) interface{} {
+		return i.(int) * 100
+	}).Filter(func(i interface{}) bool {
+		return i.(int) > 3
+	}).Limit(100).Count())
+}
+
+func Test_Distinct(t *testing.T) {
+	//raw := []Value{{val: 1}, {val: 2}, {val: 2}, {val: 1}}
+	v1 := &Value{1}
+	v2 := &Value{2}
+	raw := []*Value{v1, v2, v2}
+	stream, err := streaming.Of(raw)
+	if err != nil {
+		return
+	}
+	stream.Distinct().ForEach(func(i interface{}) {
+		fmt.Printf("%v\n", i)
+	})
+}
---
+++ streaming/streaming.go
@@ -0,0 +1,124 @@
+package streaming
+
+import (
+	"errors"
+	"reflect"
+)
+
+var empty struct{}
+
+var emptyStream = &Stream{}
+
+var streamErr = errors.New("input must be a slice or an array")
+
+// Slice alias of interface slice
+type Slice []interface{}
+
+// Stream Slice holder
+type Stream struct {
+	slice Slice
+}
+
+// Of wraps input into *Stream
+func Of(raw interface{}) (*Stream, error) {
+	switch reflect.TypeOf(raw).Kind() {
+	case reflect.Slice, reflect.Array:
+	default:
+		return nil, streamErr
+	}
+
+	var slice Slice
+	value := reflect.ValueOf(raw)
+	for i := 0; i < value.Len(); i++ {
+		ele := value.Index(i)
+		slice = append(slice, ele.Interface())
+	}
+
+	return &Stream{slice: slice}, nil
+}
+
+// ForEach performs an action for each element of this stream.
+func (s *Stream) ForEach(act func(interface{})) {
+	for _, v := range s.slice {
+		act(v)
+	}
+}
+
+// Map Returns a stream consisting of the results of applying the given
+// function to the elements of this stream.
+func (s *Stream) Map(apply func(interface{}) interface{}) *Stream {
+	var slice Slice
+	for _, v := range s.slice {
+		slice = append(slice, apply(v))
+	}
+	return &Stream{slice: slice}
+}
+
+// Limit returns a stream consisting of the elements of this stream,
+// truncated to be no longer than max-size in length.
+func (s *Stream) Limit(n int) *Stream {
+	length := len(s.slice)
+	if n > length {
+		n = length
+	}
+	return &Stream{slice: s.slice[:n]}
+}
+
+// Reduce performs a reduction on the elements of this stream,
+// using the provided combining functions
+// NOTE when steam is empty, Reduce returns -1 as the index
+func (s *Stream) Reduce(combine func(a, b interface{}) bool) (interface{}, int) {
+	if len(s.slice) == 0 {
+		return nil, -1
+	}
+
+	t := s.slice[0]
+	i := 0
+	for j, v := range s.slice {
+		if combine(v, t) {
+			t = v
+			i = j
+		}
+	}
+	return t, i
+}
+
+// Filter returns a stream consisting of the elements of this stream
+// that match the given predicate.
+func (s *Stream) Filter(predicate func(interface{}) bool) *Stream {
+	var slice Slice
+	for _, v := range s.slice {
+		if predicate(v) {
+			slice = append(slice, v)
+		}
+	}
+	return &Stream{slice: slice}
+}
+
+// Distinct returns a stream consisting of the distinct elements
+func (s *Stream) Distinct() *Stream {
+	if len(s.slice) == 0 {
+		return emptyStream
+	}
+
+	var m = make(map[interface{}]struct{})
+	for _, v := range s.slice {
+		m[v] = empty
+	}
+
+	var slice Slice
+	for k := range m {
+		slice = append(slice, k)
+	}
+	return &Stream{slice: slice}
+}
+
+// Collect returns data load of this stream
+func (s *Stream) Collect() Slice {
+	return s.slice
+}
+
+// Count returns the count of elements in this stream
+func (s *Stream) Count() int {
+	return len(s.slice)
+}
