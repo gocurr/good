d764bf6206a29d3ae78207c72516ebf99c229a53
jes <sample@gmail.com>
2021-12-24 23:01:29 +0800

    httpclient timeout

--- examples/httpclient_test.go
+++ examples/httpclient_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gocurr/good/httpclient"
 	log "github.com/sirupsen/logrus"
 	"testing"
+	"time"
 )
 
 func Test_HttpClient(t *testing.T) {
@@ -12,10 +13,17 @@ func Test_HttpClient(t *testing.T) {
 	}
 	var m = msg{Text: "great"}
 	var out msg
-	err := httpclient.PostJSON("http://127.0.0.1:9091", m, &out)
+	url := "http://127.0.0.1:9091"
+	err := httpclient.PostJSON(url, m, &out, time.Second)
 	if err != nil {
 		log.Error(err)
-		return
 	}
 	log.Info(out)
+
+	raw, err := httpclient.GetRaw(url, time.Millisecond)
+	if err != nil {
+		log.Error(err)
+	}
+
+	log.Info(string(raw))
 }
--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -2,11 +2,13 @@ package httpclient
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gocurr/good/consts"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // handleResp handles http-response
@@ -27,25 +29,41 @@ func handleResp(r *http.Response) ([]byte, error) {
 
 // PostJSONRaw calls http.Post
 // to return byte slice and reports error
-func PostJSONRaw(url string, in interface{}) ([]byte, error) {
-	all, err := json.Marshal(in)
+func PostJSONRaw(url string, in interface{}, timeout ...time.Duration) ([]byte, error) {
+	all, errMarshal := json.Marshal(in)
+	if errMarshal != nil {
+		return nil, errMarshal
+	}
+	body := bytes.NewReader(all)
+
+	var err error
+	var resp *http.Response
+	if len(timeout) > 0 {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), timeout[0])
+		defer cancelFunc()
+
+		var req *http.Request
+		req, err = http.NewRequestWithContext(ctx, http.MethodPost, url, body)
+		if err != nil {
+			return nil, err
+		}
+		resp, err = http.DefaultClient.Do(req)
+	} else {
+		resp, err = http.Post(url, consts.JSONContentType, body)
+	}
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := http.Post(url, consts.JSONContentType, bytes.NewReader(all))
-	if err != nil {
-		return nil, err
-	}
-	return handleResp(response)
+	return handleResp(resp)
 }
 
 // PostJSON posts JSON format data to the given url,
 // then unmarshal body of response into out and reports error
 //
 // Assert out is a pointer
-func PostJSON(url string, in interface{}, out interface{}) error {
-	raw, err := PostJSONRaw(url, in)
+func PostJSON(url string, in interface{}, out interface{}, timeout ...time.Duration) error {
+	raw, err := PostJSONRaw(url, in, timeout...)
 	if err != nil {
 		return err
 	}
@@ -55,20 +73,35 @@ func PostJSON(url string, in interface{}, out interface{}) error {
 
 // GetRaw calls http.Get to the given url
 // to return byte slice and reports error
-func GetRaw(url string) ([]byte, error) {
-	response, err := http.Get(url)
+func GetRaw(url string, timeout ...time.Duration) ([]byte, error) {
+	var err error
+	var resp *http.Response
+	if len(timeout) > 0 {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), timeout[0])
+		defer cancelFunc()
+
+		var req *http.Request
+		req, err = http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		if err != nil {
+			return nil, err
+		}
+		resp, err = http.DefaultClient.Do(req)
+	} else {
+		resp, err = http.Get(url)
+	}
 	if err != nil {
 		return nil, err
 	}
-	return handleResp(response)
+
+	return handleResp(resp)
 }
 
 // GetJSON calls http.Get to the given url,
 // then unmarshal body of response into out and reports error
 //
 // Assert out is a pointer
-func GetJSON(url string, out interface{}) error {
-	raw, err := GetRaw(url)
+func GetJSON(url string, out interface{}, timeout ...time.Duration) error {
+	raw, err := GetRaw(url, timeout...)
 	if err != nil {
 		return err
 	}
