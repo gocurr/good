06b0d4e179d25abc40bbd1c493fe6f3bfba899c8
Jes Cok <xigua67damn@gmail.com>
2022-04-07 22:58:12 +0800

    main.go

--- main/main.go
+++ main/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/gocurr/good/conf"
+	_ "github.com/gocurr/good/conf"
 	_ "github.com/gocurr/good/crontab"
 	_ "github.com/gocurr/good/crypto"
 	_ "github.com/gocurr/good/httpclient"
@@ -12,7 +9,7 @@ import (
 	_ "github.com/gocurr/good/mysql"
 	_ "github.com/gocurr/good/oracle"
 	_ "github.com/gocurr/good/postgres"
-	"github.com/gocurr/good/redis"
+	_ "github.com/gocurr/good/redis"
 	_ "github.com/gocurr/good/rocketmq"
 	_ "github.com/gocurr/good/server"
 	_ "github.com/gocurr/good/sugar"
@@ -21,18 +18,5 @@ import (
 
 // For pkg-updates only.
 func main() {
-	c, err := conf.New("../app.yaml")
-	if err != nil {
-		panic(err)
-	}
 
-	rdb, err := redis.New(c)
-	if err != nil {
-		panic(err)
-	}
-	s, err := rdb.Ping(context.Background()).Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(s)
 }
